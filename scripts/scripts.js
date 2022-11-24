ed0f97e7243c40968f934940012580d945e89a31
lovbackan <simon@lovbacka.se>
2022-11-24 14:32:35 +0100

    popup blur try

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,11 +11,20 @@ hamburgerBtn.addEventListener("click", () => {
 const popUp = document.querySelector(".popUp");
 const memberButtons = document.querySelectorAll("button");
 const main = document.querySelector("main");
+const body = document.querySelector("body");
     
 
 memberButtons.forEach((button) => {
     button.addEventListener("click", () => {
         popUp.classList.toggle("active");
+        // main.style.filter="blur(8px)";
+        // body.style.filter="blur(8px)";
+        // popUp.style.filter="blur(0px)";
+        // main.style.opacity="0.5";
+        // body.style.opacity="0.5";
+        // popUp.style.opacity="1";
+        
+    
     })
     
 });
