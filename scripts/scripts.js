8d6b18767e6a0de903d227183617280edddbabc0
lovbackan <simon@lovbacka.se>
2022-11-24 13:47:07 +0100

    fix popup style

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,34 @@
 
-const hamburgerBtn = document.querySelector(".hamburger")
-const dropDownMenu = document.querySelector(".dropDown")
+const hamburgerBtn = document.querySelector(".hamburger");
+const dropDownMenu = document.querySelector(".dropDown");
 
 hamburgerBtn.addEventListener("click", () => {
     dropDownMenu.classList.toggle("active")
-})
+});
+
+
+
+// const memberBtn = document.querySelector(".mobileButton");
+
+// memberBtn.addEventListener("click", () => {
+//     popUp.classList.toggle("active");
+// });
+
+const popUp = document.querySelector(".popUp");
+
+console.log(popUp);
+
+const memberButtons = document.querySelectorAll("button");
+console.log(memberButtons);
+    
+
+memberButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        popUp.classList.toggle("active");
+    })
+    
+});
+
+
+
+   
